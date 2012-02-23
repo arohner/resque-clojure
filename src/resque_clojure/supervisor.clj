d8bc80989e926ab4f70eefecd8f572e2de0036d8
Allen Rohner <arohner@gmail.com>
2012-02-23 14:00:49 -0800

    granular queue worker configuration

--- src/resque_clojure/supervisor.clj
+++ src/resque_clojure/supervisor.clj
@@ -5,31 +5,32 @@
 (def run-loop? (ref true))
 (def working-agents (ref #{}))
 (def idle-agents (ref #{}))
-(def watched-queues (atom []))
+
+(def watched-queues (ref {}))
 
 (def config (atom {:max-shutdown-wait (* 10 1000) ;; milliseconds
-                   :poll-interval (* 5 1000)
-                   :max-workers 1}))
+                   :poll-interval (* 5 1000)}))
 
 (declare release-worker reserve-worker make-agent listen-to listen-loop dispatch-jobs)
 
 (defn configure [c]
   (swap! config merge c))
 
-(defn start-agents-map [queues]
+(defn get-queues [agent]
+  (get @watched-queues agent))
+
+(defn set-queues [agent queues]
+  (dosync
+   (alter watched-queues update-in [agent] (constantly queues))))
+
+(defn start-agents [queues]
   (doseq [[q count] queues]
     (dotimes [i count]
       (make-agent :queues q))))
 
-(defn start-agents [queues]
-  (if (map? queues)
-    (start-agents-map queues)
-    (start-agents-map {queues (-> @config :max-workers)})))
-
 (defn start [queues]
   "start listening for jobs on queues (vector)."
   (start-agents queues)
-  (listen-to queues)
   (dosync (ref-set run-loop? true))
   (.start (Thread. listen-loop)))
 
@@ -38,17 +39,15 @@
   (dosync (ref-set run-loop? false))
   (apply await-for (:max-shutdown-wait @config) @working-agents))
 
-(defn make-worker-complete-fn [& {:keys [queues]}]
-  (fn worker-complete [key ref old-state new-state]
-    (release-worker ref)
-    (dispatch-jobs :queues queues)
-    (if (= :error (:result new-state))
-      (resque/report-error new-state))))
+(defn worker-complete [key agent old-state new-state]
+  (release-worker agent)
+  (dispatch-jobs :queues (get-queues agent))
+  (if (= :error (:result new-state))
+    (resque/report-error new-state)))
 
 (defn dispatch-jobs [& {:keys [queues]}]
   (when-let [worker-agent (reserve-worker)]
-    (let [msg (resque/dequeue (or queues @watched-queues))]
-      (inspect msg)
+    (let [msg (resque/dequeue (get-queues worker-agent))]
       (if msg
         (send-off worker-agent worker/work-on msg)
         (release-worker worker-agent)))))
@@ -62,29 +61,26 @@
 
 (defn make-agent [& {:keys [queues]}]
   (let [worker-agent (agent {} :error-handler (fn [a e] (throw e)))]
-    (add-watch worker-agent 'worker-complete (make-worker-complete-fn :queues queues))
+    (set-queues worker-agent queues)
+    (add-watch worker-agent :worker-complete worker-complete)
     (dosync (commute idle-agents conj worker-agent))
     worker-agent))
 
 (defn reserve-worker []
   "either returns an idle worker or nil.
    marks the returned worker as working."
-
-  (dosync
-   (let [selected (first @idle-agents)]
-     (if selected
-       (do
-         (alter idle-agents disj selected)
-         (alter working-agents conj selected)))
-     selected)))
+  (when-let [w (first @idle-agents)]
+    (dosync
+     (alter idle-agents disj w)
+     (alter working-agents conj w))
+    (resque/register (get-queues w))
+    w))
 
 (defn release-worker [w]
   (dosync (alter working-agents disj w)
-          (alter idle-agents conj w)))
-
-(defn listen-to [queues]
-  (resque/register queues)
-  (swap! watched-queues into queues))
+          (alter idle-agents conj w))
+  (resque/unregister (get-queues w))
+  w)
 
 ;; Runtime.getRuntime().addShutdownHook(new Thread() {
 ;;     public void run() { /*
