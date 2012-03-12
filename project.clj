630a1ef0c3604650d4f2f50ed2f51a8a2b52ffb9
Allen Rohner <arohner@gmail.com>
2012-03-12 13:43:37 -0700

    Add circleci qualifier to make it easy to 'lein push', bump the version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject resque-clojure "0.2.2"
+(defproject circleci/resque-clojure "0.2.3"
   :description "Redis based library for asynchronous processing"
   :url "https://github.com/jxa/resque-clojure"
   :dependencies [[org.clojure/data.json "0.1.2"]
