f2cdbb8af948681ed14734a1fe1e3325f82f8ebc
Allen Rohner <arohner@gmail.com>
2012-03-15 15:47:30 -0700

    Add clojure.tools.logging as a dep

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/jxa/resque-clojure"
   :dependencies [[org.clojure/data.json "0.1.2"]
                  [clj-time "0.3.4"]
-                 [redis.clients/jedis "2.0.0"]]
+                 [redis.clients/jedis "2.0.0"]
+                 [clojure.tools.logging "0.2.3"]]
   :dev-dependencies [[org.clojure/clojure "1.3.0"]])
 
