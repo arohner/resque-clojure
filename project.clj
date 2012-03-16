c479997f32fa5375b483471b9830ceb08e496579
Allen Rohner <arohner@gmail.com>
2012-03-16 16:32:45 -0700

    Fix the tools.logging dependency

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :dependencies [[org.clojure/data.json "0.1.2"]
                  [clj-time "0.3.4"]
                  [redis.clients/jedis "2.0.0"]
-                 [clojure.tools.logging "0.2.3"]]
+                 [org.clojure/tools.logging "0.2.3"]]
   :dev-dependencies [[org.clojure/clojure "1.3.0"]])
 
