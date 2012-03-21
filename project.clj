ff435fc90180531c068cccbdd26935e529d047e6
Allen Rohner <arohner@gmail.com>
2012-03-21 01:58:57 -0700

    Bump again, just to make sure

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject circleci/resque-clojure "0.2.5"
+(defproject circleci/resque-clojure "0.2.6"
   :description "Redis based library for asynchronous processing"
   :url "https://github.com/jxa/resque-clojure"
   :dependencies [[org.clojure/data.json "0.1.2"]
